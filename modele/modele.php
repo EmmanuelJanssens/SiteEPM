fb41c72f5c50947f0ea4c44821f0d657ade5ea1f
Emmanuel Janssens <emmanueljanssens72@gmail.com>
2017-09-12 13:54:19 +0200

    Base

---
+++ modele/modele.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emmanuel.JANSSENS
+ * Date: 05.09.2017
+ * Time: 14:02
+ */
+
+// connexion au serveur MySQL et à la BD
+// sortie : $connexion
+function getBD() {
+    $connexion = new PDO('mysql:host=localhost;dbname=cpm;charset=utf8', 'root', '');
+    // permet d'avoir plus de détails sur les erreurs retournées
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $connexion;
+
+}
+
+//Fonction : vérifie le login de l'utilisateur
+//Sortie : résultat de la requête
+function getPwdFromLogin($login)
+{
+    $connexion = getBD();
+    $requete = "SELECT idUser, pwd FROM Rights WHERE login='" . $login . "'";
+    $resultats = $connexion->query($requete);
+    if ($donnees = $resultats->fetch()) {
+        return $donnees['pwd'];
+    } else {
+        return '';
+    }
+}
+
+
+?>
